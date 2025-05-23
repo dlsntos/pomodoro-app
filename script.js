7a98509658c599a1d5094b0a6ca6d039c088f389
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-05-23 20:38:51 +0800

    Added a appTimer function

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const startBtn = document.querySelector('.btn-start');
 const session = document.querySelector('.minutes');
 let myInterval;
 let state = true;
+
+const appTimer = () => {
+    const sessionAmount = Number.parseInt(session.textContent);
+
+    if(state) {
+        state = false;
+        let totalSeconds = sessionAmount * 60;
+        
+        const updateSeconds = () => {
+
+        }
+        myInterval = setInterval(updateSeconds, 1000);
+    } else {
+        alert('Session has already started.');
+    }
+}
