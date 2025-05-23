55c92b926a01ffee1f578f822cd65202ec532739
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-05-23 13:03:44 +0800

    Add new sound file and modified sound name

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bells = new Audio('./sounds/bells.wav');
+const bells = new Audio('./sounds/bell.mp3');
 const startBtn = document.querySelector('.btn-start');
 const session = document.querySelector('.minutes');
 let myInterval;
